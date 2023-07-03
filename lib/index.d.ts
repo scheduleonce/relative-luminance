89d70e344ce70c9b33055642631d9f0021c5c4aa
Michael DeRazon <michaeld@scheduleonce.com>
2023-07-03 23:10:44 +0100

    add typings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,16 @@
-/**
- * Create an order id object
- *
- * @param {string} colorCode A color code
- */
-declare function luminance(colorCode: string): string {};
+declare module "relativeLuminance" {
+  /**
+   * Calculates the luminance of a given hex color code.
+   * @param colorCode The hex color code to calculate the luminance of.
+   * @returns Either "dark" or "light" depending on the luminance of the color.
+   */
+  function relativeLuminance(
+    colorCode: string
+  ):
+    | "dark"
+    | "light"
+    | "Please provide a hex color code"
+    | "Please provide a valid hex code";
 
-export default luminance;
+  export = relativeLuminance;
+}
