9e26b33f0a9de886a1e5661ff40a96e103db0964
Michael DeRazon <michaeld@scheduleonce.com>
2023-07-04 10:11:36 +0100

    update api to throw instead of returning string

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,14 +3,7 @@ declare module "relativeLuminance" {
    * Calculates the luminance of a given hex color code.
    * @param colorCode The hex color code to calculate the luminance of.
    * @returns Either "dark" or "light" depending on the luminance of the color.
+   * @throws Will throw an error if colorCode is invalid or undefined.
    */
-  function relativeLuminance(
-    colorCode: string
-  ):
-    | "dark"
-    | "light"
-    | "Please provide a hex color code"
-    | "Please provide a valid hex code";
-
-  export = relativeLuminance;
+  export function relativeLuminance(colorCode: string): "dark" | "light";
 }
