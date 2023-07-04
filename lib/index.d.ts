040494ea902607f8e2f2801d0c477954ae26eda8
Michael DeRazon <michaeld@oncehub.com>
2023-07-04 11:00:25 +0100

    fix type definitions

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,9 +1,17 @@
-declare module "relativeLuminance" {
-  /**
-   * Calculates the luminance of a given hex color code.
-   * @param colorCode The hex color code to calculate the luminance of.
-   * @returns Either "dark" or "light" depending on the luminance of the color.
-   * @throws Will throw an error if colorCode is invalid or undefined.
-   */
-  export function relativeLuminance(colorCode: string): "dark" | "light";
-}
+/**
+Calculates the luminance of a given hex color code.
+@param colorCode The hex color code to calculate the luminance of.
+@returns Either "dark" or "light" depending on the luminance of the color.
+@throws Will throw an error if colorCode is invalid or undefined.
+@example
+```js
+import luminance from '@oncehub/relative-luminance';
+
+luminance("#000000");
+//=> dark
+
+luminance("#ffffff");
+//=> light
+```
+ */
+export default function relativeLuminance(colorCode: string): "dark" | "light";
